c31e28a1ea72c5d167d3c9f1280d1482eb599585
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-30 17:47:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+  document.addEventListener('DOMContentLoaded', function () {
+      // DOM is loaded, write the string
+      const body = document.querySelector('body');
+      body.textContent = 'DOM load success';
+    });
